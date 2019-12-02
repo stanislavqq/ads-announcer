11b85a42f5fef069e2f968b32af69d8ba17bc405
Stanislav QQ <stanislavqq@yandex.ru>
2019-12-02 20:27:33 +0600

    add home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel</title>
+    <script src="{{ route('assets.lang') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div id="app"></div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
